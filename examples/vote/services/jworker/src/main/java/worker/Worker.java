21902147e897b03e92d18a0df32974abd9d0d791
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2018-12-05 21:16:11 +0100

    chore: tweaks to vote project
    * Add missing service dependencies to the relevant modules' config.
    
    * Added a health check to the db (postgres) service, and moved the
    table creation to a task. Removed logic from jworker accordingly.

--- examples/vote/services/jworker/src/main/java/worker/Worker.java
+++ examples/vote/services/jworker/src/main/java/worker/Worker.java
@@ -66,23 +66,9 @@ class Worker {
     Connection conn = null;
 
     try {
-
       Class.forName("org.postgresql.Driver");
       String url = "jdbc:postgresql://" + host + "/postgres";
-
-      while (conn == null) {
-        try {
-          conn = DriverManager.getConnection(url, "postgres", "");
-        } catch (SQLException e) {
-          System.err.println("Waiting for db");
-          sleep(1000);
-        }
-      }
-
-      PreparedStatement st = conn.prepareStatement(
-        "CREATE TABLE IF NOT EXISTS votes (id VARCHAR(255) NOT NULL UNIQUE, vote VARCHAR(255) NOT NULL, created_at timestamp default NULL )");
-      st.executeUpdate();
-
+      conn = DriverManager.getConnection(url, "postgres", "");
     } catch (ClassNotFoundException e) {
       e.printStackTrace();
       System.exit(1);
